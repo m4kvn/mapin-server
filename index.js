925e6231c5ad025564a7d637e3c0661d94d6b4c4
manohito <fun.masahiro@gmail.com>
2017-11-05 20:54:34 +0900

    Change port to 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
